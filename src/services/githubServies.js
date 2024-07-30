779502e3ab4930f82c876ab6efc1e30fcce401ed
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-07-30 13:04:47 +0500

    first commit

--- src/services/githubServies.js
+++ src/services/githubServies.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 
 const BASE_URL = "https://api.github.com"
+const GITHUB_TOKEN = "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"
 
-
-axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`
+axios.defaults.headers.common['Authorization'] = `Bearer ${GITHUB_TOKEN}`
 
 const searchUsers = (queries) => {
     return axios.get(`${BASE_URL}/search/users`, {
